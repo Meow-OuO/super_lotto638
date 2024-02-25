450697a6d8f2c1e45052a3d6e1ab766533afd3f7
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-02-25 18:04:39 +0800

    Update rand_S638.js

--- js/rand_S638.js
+++ js/rand_S638.js
@@ -13,5 +13,5 @@ function roll() {
 			random = rand(num);
 		}
 	}
-document.getElementById("S_lotto638").innerHTML='第一區:'+"&nbsp;"+arr.sort(function(a, b){return a-b})+"&emsp;"+'第二區:'+"&nbsp;"+rand(8)
+document.getElementById("S_lotto638").innerHTML='第一區:'+"&nbsp;"+arr.sort(function(a, b){return a-b})+"&emsp;"+'第二區:'+"&nbsp;"+rand(8);
 }
