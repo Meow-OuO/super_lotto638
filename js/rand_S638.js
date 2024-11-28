0f00d3196d737e67e19a9a4e2ff6503130240673
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-11-29 03:08:48 +0800

    Update rand_S638.js

--- js/rand_S638.js
+++ js/rand_S638.js
@@ -3,21 +3,19 @@ function rand(max) {
 }
 
 function roll() {
-  let num = 38;  // 第一區的最大數字
+  let num = 38;
   let arr = [];
   
   // 產生 6 個唯一的隨機數字
   while (arr.length < 6) {
     let random = rand(num);
     if (!arr.includes(random)) {
-      arr.push(random);  // 如果數字不在陣列中，則加入
+      arr.push(random);
     }
   }
 
-  // 將數字按升序排序
   arr.sort(function(a, b) { return a - b; });
 
-  // 更新網頁顯示內容
   document.getElementById("S_lotto638").innerHTML = 
     '第一區:' + "&nbsp;" + arr.join(", ") + "&emsp;" + '第二區:' + "&nbsp;" + rand(8);
 }
